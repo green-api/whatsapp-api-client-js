49771edd88488305e6d9d558ef5ce55345cebae4
rkudakov <rkudakov@bit-erp.ru>
2020-05-13 22:45:12 +0300

    GR-7 Библиотека ГринАПИ - npm

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,24 @@
 import { terser } from "rollup-plugin-terser";
+import progress from 'rollup-plugin-progress';
 
 module.exports = {
     input: 'src/index.js',
-    output: {
-        file: 'lib/bundle.js',
-        format: 'umd',
-        exports: 'default',
-        name: 'whatsAppClient'
-    },
-    plugins: [terser()]
+    output: [
+        {
+            file: 'lib/bundle.js',
+            format: 'cjs'
+        },
+        {
+            file: 'lib/whats-app-client.min.js',
+            format: 'umd',
+            exports: 'default',
+            name: 'whatsAppClient',
+            plugins: [terser()]
+        }
+    ],
+    plugins: [
+        progress({
+            clearLine: false // default: true
+        })
+    ]
 };
