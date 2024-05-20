0da1ceb9420714fd0f0b5fb979ecf30a9957096e
tkulzhan <tamirlankulzhanov@gmail.com>
2024-05-20 13:46:07 +0500

    requested changes applied

--- examples/getAuthorizationCode.js
+++ examples/getAuthorizationCode.js
@@ -1,19 +1,20 @@
 const whatsAppClient = require('@green-api/whatsapp-api-client')
 
 // instance manager https://console.green-api.com
-const idInstance = ''; // your instance id
-const apiTokenInstance = ''; // your instance api token
-const phoneNumber = 12345678910 // phone number to authorize
+const idInstance = '1101000001'; // your instance id
+const apiTokenInstance = 'd75b3a66374942c5b3c019c698abc2067e151558acbd412345'; // your instance api token
+const phoneNumber = 12345678910; // phone number to authorize
 
 (async () => {
     const restAPI = whatsAppClient.restAPI(({
-        idInstance,
-        apiTokenInstance
+        idInstance, apiTokenInstance
     }))
+
     try {
-        const response = await restAPI.instance.getAuthorizationCode(phoneNumber)
-        console.log(response)
+        const getAuthorizationCodeResponse = await restAPI.instance.getAuthorizationCode(phoneNumber)
+
+        console.log(getAuthorizationCodeResponse)
     } catch (ex) {
         console.error(ex);
     }
-})();s
+})();
